1a149bfb8c1c56ea5c9bac6b40b2727d5c56c90d
Glen Chiacchieri <www@glench.com>
2019-04-01 16:58:34 -0400

    add new_line
    still need to get working transparency, clicking

--- play/play.py
+++ play/play.py
@@ -7,6 +7,7 @@ _warnings.formatwarning = warning_format
 
 
 import pygame
+import pygame.gfxdraw
 
 import asyncio as _asyncio
 import random as _random
@@ -349,19 +350,19 @@ You might want to look in your code where you're setting transparency and make s
             self.x = x
             self.y = y 
 
-    def distance_to(self, x=None, y=None):
+    def distance_to(self, x, y=None):
         assert(not x is None)
 
         try:
-            # x can either by a number or a sprite. If it's a sprite:
-            x = x.x
-            y = x.y
+            # x can either be a number or a sprite. If it's a sprite:
+            x1 = x.x
+            y1 = x.y
         except AttributeError:
-            x = sprite_or_x
-            y = y
+            x1 = x
+            y1 = y
 
-        dx = self.x - x
-        dy = self.y - y
+        dx = self.x - x1
+        dy = self.y - y1
 
         return _math.sqrt(dx**2 + dy**2)
 
@@ -569,7 +570,6 @@ class circle(sprite):
             pygame.draw.circle(self._primary_pygame_surface, _color_name_to_rgb(self._border_color), (center, center), self._radius)
             # draw fill circle over border circle
             pygame.draw.circle(self._primary_pygame_surface, _color_name_to_rgb(self._color), (center, center), self._radius-self._border_width)
-
         else:
             pygame.draw.circle(self._primary_pygame_surface, _color_name_to_rgb(self._color), (center, center), self._radius)
 
@@ -616,6 +616,136 @@ class circle(sprite):
         self._border_width = _border_width
         self._should_recompute_primary_surface = True
 
+def new_line(color='black', x=0, y=0, length=None, angle=None, thickness=1, x1=None, y1=None, transparency=100, size=100):
+    return line(color=color, x=x, y=y, length=length, angle=angle, thickness=thickness, x1=x1, y1=y1, transparency=transparency, size=size)
+
+class line(sprite):
+    def __init__(self, color='black', x=0, y=0, length=None, angle=None, thickness=1, x1=None, y1=None, transparency=100, size=100):
+        self.x = x
+        self.y = y
+        self._color = color
+        self._thickness = thickness
+
+        # can set either (length, angle) or (x1,y1), otherwise a default is used
+        if length != None and angle != None:
+            self._length = length
+            self._angle = angle
+            self._x1, self._y1 = self._calc_endpoint()
+        elif x1 != None and y1 != None:
+            self._x1 = x1
+            self._y1 = y1
+            self._length, self._angle = self._calc_length_angle()
+        else:
+            # default values
+            self._length = length or 100
+            self._angle = angle or 0
+            self._x1, self._y1 = self._calc_endpoint()
+
+        self._transparency = transparency
+        self._size = size
+        self._is_hidden = False
+
+        self._when_clicked_callbacks = []
+
+        self._compute_primary_surface()
+
+        all_sprites.append(self)
+
+    def clone(self):
+        return self.__class__(color=self.color, length=self.length, thickness=self.thickness, **self._common_properties())
+
+    def _compute_primary_surface(self):
+        width = max(abs(self.x1-self.x), self.thickness)
+        height = max(abs(self.y1-self.y), self.thickness)
+
+        self._primary_pygame_surface = pygame.Surface((width, height), pygame.SRCALPHA)
+        # line is actually draw in _game_loop because coordinates work different
+
+        self._should_recompute_primary_surface = False
+        self._compute_secondary_surface(force=True)
+
+    def _compute_secondary_surface(self, force=False):
+        self._secondary_pygame_surface = self._primary_pygame_surface.copy()
+
+        if force or self._transparency != 100:
+            self._secondary_pygame_surface.set_alpha(round((self._transparency/100.) * 255))
+
+        self._should_recompute_secondary_surface = False
+
+    ##### color #####
+    @property 
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, _color):
+        self._color = _color
+        self._should_recompute_primary_surface = True
+
+    ##### thickness #####
+    @property 
+    def thickness(self):
+        return self._thickness
+
+    @thickness.setter
+    def thickness(self, _thickness):
+        self._thickness = _thickness
+        self._should_recompute_primary_surface = True
+
+    def _calc_endpoint(self):
+        radians = _math.radians(self._angle)
+        return self._length * _math.cos(radians) + self.x, self._length * _math.sin(radians) + self.y
+
+    ##### length #####
+    @property 
+    def length(self):
+        return self._length
+
+    @length.setter
+    def length(self, _length):
+        self._length = _length
+        self._x1, self._y1 = self._calc_endpoint()
+        self._should_recompute_primary_surface = True
+
+    ##### angle #####
+    @property 
+    def angle(self):
+        return self._angle
+
+    @angle.setter
+    def angle(self, _angle):
+        self._angle = _angle
+        self._x1, self._y1 = self._calc_endpoint()
+        self._should_recompute_primary_surface = True
+
+
+    def _calc_length_angle(self):
+        dx = self.x - self.x1
+        dy = self.y - self.y1
+
+        return _math.sqrt(dx**2 + dy**2), _math.degrees(_math.atan2(y-self.y, x-self.x))
+
+    ##### x1 #####
+    @property 
+    def x1(self):
+        return self._x1
+
+    @x1.setter
+    def x1(self, _x1):
+        self._x1 = _x1
+        self._length, self._angle = self._calc_length_angle()
+        self._should_recompute_primary_surface = True
+
+    ##### y1 #####
+    @property 
+    def y1(self):
+        return self._y1
+
+    @y1.setter
+    def y1(self, _y1):
+        self._angle = _y1
+        self._length, self._angle = self._calc_length_angle()
+        self._should_recompute_primary_surface = True
 
 def new_text(words='hi :)', x=0, y=0, font=None, font_size=50, color='black', angle=0, transparency=100, size=100):
     return text(words=words, x=x, y=y, font=font, font_size=font_size, color=color, angle=angle, transparency=transparency, size=size)
@@ -656,6 +786,7 @@ To fix this, either set the font to None, or make sure you have a font file (usu
         self._should_recompute_primary_surface = False
 
         self._compute_secondary_surface(force=True)
+
 
     @property
     def words(self):
@@ -911,7 +1042,7 @@ def _game_loop():
         #################################
         # @sprite.when_clicked events
         #################################
-        if mouse.is_clicked():
+        if mouse.is_clicked() and not type(sprite) == line:
             # get_rect().collidepoint() is local coordinates, e.g. 100x100 image, so have to translate
             if sprite._secondary_pygame_surface.get_rect().collidepoint((mouse.x+screen.width/2.)-sprite._pygame_x(), (screen.height/2.-mouse.y)-sprite._pygame_y()):
                 sprite._is_clicked = True
@@ -933,7 +1064,19 @@ def _game_loop():
         elif sprite._should_recompute_secondary_surface:
             _loop.call_soon(sprite._compute_secondary_surface)
 
-        _pygame_display.blit(sprite._secondary_pygame_surface, (sprite._pygame_x(), sprite._pygame_y()) )
+        if type(sprite) == line:
+            # @hack: Line-drawing code should probably be in the line._compute_primary_surface function
+            # but the coordinates work different for lines than other sprites.
+            x = screen.width/2 + sprite.x
+            y = screen.height/2 - sprite.y
+            x1 = screen.width/2 + sprite.x1
+            y1 = screen.height/2 - sprite.y1
+            if sprite.thickness != 1:
+                 pygame.draw.line(_pygame_display, _color_name_to_rgb(sprite.color), (x,y), (x1,y1), sprite.thickness)
+            else:
+                 pygame.draw.aaline(_pygame_display, _color_name_to_rgb(sprite.color), (x,y), (x1,y1), sprite.thickness)
+        else:
+            _pygame_display.blit(sprite._secondary_pygame_surface, (sprite._pygame_x(), sprite._pygame_y()) )
 
     pygame.display.flip()
     _loop.call_soon(_game_loop)
